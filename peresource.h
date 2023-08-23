36480e1891b7d282a53a98edc700a99339b71062
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2023-08-23 16:42:08 +0700

    Update peresource.h

--- peresource.h
+++ peresource.h
@@ -6,3 +6,6 @@ BOOL IsPEHeader(HANDLE);
 BOOL ReadMachine(HANDLE fileHandle);
 DWORD CodeSize(HANDLE fileHandle);
 DWORD EntryPointAddress(HANDLE fileHandle);
+
+PIMAGE_NT_HEADERS ReadPEHeader(HANDLE);
+PIMAGE_DOS_HEADER ReadDOSHeader(HANDLE);
